d7a5ef474df09c9201de6a3d61bd546d720feee6
cburdine <colinburdine443@gmail.com>
2019-03-17 23:39:48 -0500

    Queried a sample value from firebase

--- BeMyPlayer2/src/firebase/FirebaseTestConnector.java
+++ BeMyPlayer2/src/firebase/FirebaseTestConnector.java
@@ -3,9 +3,18 @@ package firebase;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import java.sql.Date;
+import java.util.List;
+
+import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.cloud.Service;
 
 public class FirebaseTestConnector {
 	
@@ -24,9 +33,32 @@ public class FirebaseTestConnector {
 			    .build();
 
 			FirebaseApp.initializeApp(options);
-		} catch (IOException e) {
+			
+			System.out.println("Connection initialized.\nQuerying db...");
+			
+			Firestore db = FirestoreClient.getFirestore();
+			
+			// asynchronously retrieve all users
+			ApiFuture<QuerySnapshot> query = db.collection("users").get();
+			// ...
+			// query.get() blocks on response
+			QuerySnapshot querySnapshot = query.get();
+			List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+			
+			for (QueryDocumentSnapshot document : documents) {
+			  System.out.println("User: " + document.getId());
+			  System.out.println("First: " + document.getString("first"));
+			  if (document.contains("middle")) {
+			    System.out.println("Middle: " + document.getString("middle"));
+			  }
+			  System.out.println("Last: " + document.getString("last"));
+			  System.out.println("Born: " + document.getLong("born"));
+			}
+			
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 }
--- BeMyPlayer2/src/module-info.java
+++ BeMyPlayer2/src/module-info.java
@@ -7,6 +7,10 @@
  */
 module bemyplayer2 {
 	requires java.desktop;
-	requires firebase.admin;
 	requires google.auth.library.oauth2.http;
+	requires firebase.admin;
+	requires java.sql;
+	requires api.common;
+	requires google.cloud.firestore;
+	requires google.cloud.core;
 }
