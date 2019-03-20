cf2fe291b863531b7d14ab6973577d3bb31fd17d
Ben Davis <ben_davis3@baylor.edu>
2019-03-19 21:10:10 -0500

    fixed sizing issue on swipe -Ben

--- BeMyPlayer2/src/graphics/SwipePageModel.java
+++ BeMyPlayer2/src/graphics/SwipePageModel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class SwipePageModel {
 	
@@ -35,7 +36,15 @@ public class SwipePageModel {
 		
 		this.left = new SwipeButton(new SwipeLeftController());
 		this.right = new SwipeButton(new SwipeRightController());
-
+		
+		layout.setHgap(20);
+		layout.setVgap(15);
+		JLabel lblBeMyPlayer = new JLabel("Be My Player 2");
+		lblBeMyPlayer.setFont(new Font("Monospaced", Font.BOLD, 20));
+		lblBeMyPlayer.setForeground(yellow);
+		lblBeMyPlayer.setBounds(160,0,204,69);
+		
+		frame.getContentPane().add(lblBeMyPlayer, BorderLayout.NORTH);
 		frame.getContentPane().add(this.left, BorderLayout.LINE_START);
 		frame.getContentPane().add(this.right, BorderLayout.LINE_END);
 		frame.getContentPane().add(this.backButton, BorderLayout.PAGE_END);
