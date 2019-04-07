beb2c2b8476061ab0eee9cf3ac6a466f318560ec
Ben Davis <ben_davis3@baylor.edu>
2019-04-07 14:24:19 -0500

    added validation for page 3 -Ben

--- BeMyPlayer2/src/graphics/CreateAccountPageController.java
+++ BeMyPlayer2/src/graphics/CreateAccountPageController.java
@@ -22,7 +22,8 @@ import model.Profile;
 
 public class CreateAccountPageController extends PageController{
 
-	//	action commands 	
+	//	action commands
+	public static final int MAXLENGTH = 250;
 	public static final String NEXT = "next";
 	public static final String BACK="back";
 	public static final String SUBMIT = "submit";
@@ -265,8 +266,19 @@ public class CreateAccountPageController extends PageController{
 	public boolean validateCreatePage3() {
 		boolean valid = true;
 		//	need to store profile pic in new location to pull from
-		System.out.println("Profile pic "+ this.getCreateAccountPageModel().getImagePath());
-		System.out.println("description " + this.getCreateAccountPageModel().getCharDescription().getText());
+		List<String >warnings = new ArrayList<>();
+		
+		if(this.getCreateAccountPageModel().getCharDescription().getText().equals("")) {
+			valid = false;
+			warnings.add("Please enter a description\n");
+		}
+		if(this.getCreateAccountPageModel().getCharDescription().getText().length() > MAXLENGTH) {
+			valid = false;
+			warnings.add("Character limit 250 exceeded\n");
+		}
+		if(valid == false) {
+			InvalidPopup p = new InvalidPopup(this.getCreateAccountPanel(),warnings);
+		}
 		
 		//	send to temp account and populate db
 		return valid;
