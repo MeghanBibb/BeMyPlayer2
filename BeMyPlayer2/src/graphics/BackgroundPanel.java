a0b5ab922e3c952df4613b90dc9c98e3d832000e
cburdine <colinburdine443@gmail.com>
2019-04-08 20:25:00 -0500

    Fixed Lag issue- added a ResourceManager class

--- BeMyPlayer2/src/graphics/BackgroundPanel.java
+++ BeMyPlayer2/src/graphics/BackgroundPanel.java
@@ -10,27 +10,58 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import model.ResourceManager;
+
 public class BackgroundPanel extends JPanel {
+	
+	public static BufferedImage baseImage = ResourceManager.loadImage("background.png");
+	
+	
+	private Image scaledBackground;
+	private boolean updated = false;
+	private int offsetX = 0, offsetY = 0;
+	
 	public BackgroundPanel(Object object) {
 		super(null);
+		scaledBackground = baseImage;
+		updated = false;
 	}
-
+	
+	
 	@Override
 	  protected void paintComponent(Graphics g) {
 	    super.paintComponent(g);
-	  ////
-		BufferedImage backgroundImage;
-		try {
-			
-			backgroundImage = ImageIO.read(new File("background.png"));
-			g.drawImage(backgroundImage.getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_SMOOTH), 0, 0, null);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	  
+	    if(!updated && this.getHeight() > 0 && this.getWidth() > 0) {
+	    	resize();
+	    	this.updated = true;
+	    }
+		g.drawImage(scaledBackground, 0, 0, null);
+		
+	}
+	
+	private void resize() {
+		
+		offsetX = 0;
+		offsetY = 0;
+		int newWidth = 0, newHeight = 0;
+		double baseAspect = (double) baseImage.getHeight() / (double) baseImage.getWidth();
+		double fitAspect = (double) this.getHeight() / (double) this.getWidth();
+		
+		if(fitAspect > baseAspect) {
+			newHeight = this.getHeight();
+			newWidth = 1 + (int)(this.getHeight() / baseAspect);
+			offsetX = newWidth - this.getWidth();
+		}else {
+			newWidth = this.getWidth();
+			newHeight = 1 + (int)(this.getWidth() * baseAspect);
+			offsetY = newHeight - this.getHeight();
 		}
-		
-		
-		////
+				
+		scaledBackground = baseImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+	}
+	
+	public void update() {
+		updated = false;
 	}
 }
---
+++ BeMyPlayer2/src/model/ResourceManager.java
@@ -0,0 +1,66 @@
+package model;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+
+import firebase.FireBaseAdapter;
+
+public class ResourceManager {
+	
+	public static final String IMAGE_FOLDER = "src/main/resources/img/";
+	public static final String FONTS_FOLDER = "src/main/resources/fonts/";
+	
+	private static Logger LOGGER = Logger.getLogger(ResourceManager.class.getName());
+	
+	public static BufferedImage loadImage(String name) {
+		try {
+			return ImageIO.read(new File(IMAGE_FOLDER + name));
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "Failed to load image resource: " + name);
+		}
+		return null;
+	}
+	
+	public static Font loadFont(String name, float size) {
+		InputStream fontStream;
+		try {
+			fontStream = new BufferedInputStream(new FileInputStream("JetSet.ttf"));
+		} catch (FileNotFoundException e1) {
+			LOGGER.log(Level.SEVERE, "Failed to find font resource: " + name);
+			return null;
+		}
+		try {
+			return Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(Font.PLAIN, size);
+		} catch (FontFormatException | IOException e) {
+			LOGGER.log(Level.SEVERE, "Failed to load font resource: " + name);
+		}
+		return null;
+	}
+	
+	public static Font loadFont(String name) {
+		InputStream fontStream;
+		try {
+			fontStream = new BufferedInputStream(new FileInputStream("JetSet.ttf"));
+		} catch (FileNotFoundException e1) {
+			LOGGER.log(Level.SEVERE, "Failed to find font resource: " + name);
+			return null;
+		}
+		try {
+			return Font.createFont(Font.TRUETYPE_FONT, fontStream);
+		} catch (FontFormatException | IOException e) {
+			LOGGER.log(Level.SEVERE, "Failed to load font resource: " + name);
+		}
+		return null;
+	}
+}
