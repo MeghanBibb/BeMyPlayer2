a77501ed81c697730ad5ea9f54e18d779b196ce8
Sam  Muller <sam_muller1@baylor.edu>
2019-04-11 17:39:18 -0500

    Changed color yellow

--- BeMyPlayer2/src/graphics/Colors.java
+++ BeMyPlayer2/src/graphics/Colors.java
@@ -3,7 +3,7 @@ package graphics;
 import java.awt.Color;
 
 public class Colors {
-	static Color Yellow = new Color(254, 195, 123);
+	static Color Yellow = new Color(251,175,124);
 	static Color Red = new Color(134,48,111);
 	static Color White = new Color(255,255,255);
 }
