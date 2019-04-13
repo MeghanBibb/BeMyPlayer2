84abfc770cd6e3924d905f03dcdce4fb43a12e64
Ben Davis <ben_davis3@baylor.edu>
2019-04-13 15:59:35 -0500

    trying to fix profile pics -Ben

--- BeMyPlayer2/src/graphics/CreateAccountPageView.java
+++ BeMyPlayer2/src/graphics/CreateAccountPageView.java
@@ -788,10 +788,12 @@ public class CreateAccountPageView {
 					}
 					capController.getCreateAccountPageModel().setImagePath(f.getAbsolutePath());
 					capController.getCreateAccountPageModel().setProfileImg(img1);
-				}else if(f == null){
+				}else if(f == null || img1 == null){
 					img1 = CreateAccountPageModel.DEFAULT_PROFILE_IMAGE;
+					capController.getCreateAccountPageModel().setProfileImg(img1);
 					capController.getCreateAccountPageModel().setImagePath(img1.toString());
 				}
+				
 				setIcon.setIcon(new ImageIcon(new ImageIcon(img1).getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT)));
 				setIcon.setBounds(125, 25, 150, 150);
 				capController.getCreateAccountPageModel().setProfileImg(img1);
