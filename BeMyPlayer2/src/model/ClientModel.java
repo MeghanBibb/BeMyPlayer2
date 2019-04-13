3123915631b9a06a63b7b5cff07962fb19c57cc5
cburdine <colinburdine443@gmail.com>
2019-04-13 11:50:36 -0500

    Fixed font not found issue

--- BeMyPlayer2/src/model/ClientModel.java
+++ BeMyPlayer2/src/model/ClientModel.java
@@ -3,22 +3,63 @@ package model;
 import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class ClientModel {
 	
+	static int friendMatchBatch = 0;
+	static int loveMatchBatch = 0;
 	
+	private static Queue<Profile> newFriendMatchQueue = new PriorityBlockingQueue<Profile>();
+	private static Queue<Profile> newLoveMatchQueue = new PriorityBlockingQueue<Profile>();
 	
-	int friendMatchBatch = 0;
-	int loveMatchBatch = 0;
+	static List<Profile> loveMatches;
+	static List<Profile> friendMatches;
 	
-	Queue<Profile> newFriendMatchQueue;
-	Queue<Profile> newLoveMatchQueue;
+	static Map<String,BufferedImage> profileImageCache;
 	
-	List<Profile> loveMatches;
-	List<Profile> friendMatches;
+	public static void enqueueFriendProfiles(List<Profile> newFriends) {
+		newFriendMatchQueue.addAll(newFriends);
+	}
 	
-	Map<String,BufferedImage> profileImageCache;
+	public static void enqueueLoveProfiles(List<Profile> newLoves) {
+		newLoveMatchQueue.addAll(newLoves);
+	}
+	
+	public static Profile getFriendProfileFront() {
+		return newFriendMatchQueue.peek();
+	}
+	
+	public static Profile getLoveProfileFront() {
+		return newFriendMatchQueue.peek();
+	}
+	
+	public static void dequeueFriendProfile() {
+		newFriendMatchQueue.remove();
+	}
+	
+	public static void dequeLoveProfile() {
+		newLoveMatchQueue.remove();
+	}
+
+	public static List<Profile> getLoveMatches() {
+		return loveMatches;
+	}
+
+	public static void setLoveMatches(List<Profile> loveMatches) {
+		ClientModel.loveMatches = loveMatches;
+	}
+
+	public static List<Profile> getFriendMatches() {
+		return friendMatches;
+	}
+
+	public static void setFriendMatches(List<Profile> friendMatches) {
+		ClientModel.friendMatches = friendMatches;
+	}
+	
 	
 	
 	
