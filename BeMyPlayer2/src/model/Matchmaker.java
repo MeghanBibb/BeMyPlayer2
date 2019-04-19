c424f0716096ddb885625e494c81919148bd91a6
Ben Davis <ben_davis3@baylor.edu>
2019-04-19 11:49:19 -0500

    Fixed matchmaking comparator -Ben

--- BeMyPlayer2/src/model/Matchmaker.java
+++ BeMyPlayer2/src/model/Matchmaker.java
@@ -22,7 +22,25 @@ public class Matchmaker{
 			@Override
 			public int compare(Profile o1, Profile o2) {
 				//This is a temporary stub:
-				return o1.getUsername().compareTo(o2.getUsername());
+				//	user profile and compare < 0 if left more similar than right or pos if right more like
+				//lower score is better return left - right
+				return generateZScore(o1) - generateZScore(o2);
+			}
+			public int generateZScore(Profile o) {
+				int score = 0;
+				if(o.getUserId().equals(c.getUserId())) {
+					return 1000;
+				}
+				if(o.getUsername().equals(c.getUsername())) {
+					return 1000;
+				}
+				score += o.getGender().compareTo(o.getGender());
+				for(int i = 0; i < o.getPlatforms().size(); i++) {
+					if(o.getPlatforms().get(i).equals(c.getPlatforms().get(i))) {
+						score--;
+					}
+				}
+				return score;
 			}
 			
 	    };
@@ -41,7 +59,30 @@ public class Matchmaker{
 			@Override
 			public int compare(Profile o1, Profile o2) {
 				//This is a temporary stub:
-				return o1.getUsername().compareTo(o2.getUsername());
+				return generateZScore(o1) - generateZScore(o2);
+			}
+			public int generateZScore(Profile o) {
+				int score = 0;
+				if(o.getUserId().equals(c.getUserId())) {
+					return 1000;
+				}
+				if(o.getUsername().equals(c.getUsername())) {
+					return 1000;
+				}
+				if(o.getGender().equals(c.getGender())) {
+					return 1000;
+				}
+				for(int i = 0; i < o.getPlatforms().size(); i++) {
+					if(o.getPlatforms().get(i).equals(c.getPlatforms().get(i))) {
+						score--;
+					}
+				}
+				for(int i = 0; i < o.getGenres().size(); i++) {
+					if(o.getGenres().get(i).equals(c.getGenres().get(i))) {
+						score--;
+					}
+				}
+				return score;
 			}
 			
 	    };
